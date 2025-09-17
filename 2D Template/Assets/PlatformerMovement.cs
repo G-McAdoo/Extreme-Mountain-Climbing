6d21fc9a3a219cf25f087c194b03a43e4782dbef
MUKILTEO\1725607 <1725607@mukilteo.wednet.edu>
2025-09-17 10:15:54 -0700

    Added movement to player controller.

--- 2D Template/Assets/PlatformerMovement.cs
+++ 2D Template/Assets/PlatformerMovement.cs
@@ -1,18 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlatformerMovement : MonoBehaviour
 {
-    public float speed = 5;
+    public float moveSpeed;
+    public float jumpHeight;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D rb2d;
+
+    private float _movement;
+
+    void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        rb2d.linearVelocityX = _movement;
+    }
+
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        _movement = ctx.ReadValue<Vector2>().x * moveSpeed;
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1)
+        {
+            rb2d.linearVelocityY = jumpHeight;
+        }
         
     }
 }
